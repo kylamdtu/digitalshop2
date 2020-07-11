c1ca01d3bc9bed1cf5b9adbae17947a1ddbf88a7
nht1206 <nghuutho74@gmail.com>
2020-07-11 11:39:34 +0700

    fix bugs

--- src/main/java/com/dtucdio3/digitalshop2/service/ImageService.java
+++ src/main/java/com/dtucdio3/digitalshop2/service/ImageService.java
@@ -5,6 +5,8 @@ import com.dtucdio3.digitalshop2.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ImageService {
     ImageRepository imageRepository;
@@ -16,5 +18,9 @@ public class ImageService {
 
     public void save(Image image) {
         imageRepository.save(image);
+    }
+
+    public List<Image> findAll() {
+        return imageRepository.findAll();
     }
 }
